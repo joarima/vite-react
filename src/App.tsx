0cd9eb6aee553703d6a6df1f2b8f2c139f435fd1
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-16 00:16:37 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { PlateEditor } from '@/components/PlateEdtor'
+import { PlateController } from '@udecode/plate-common'
+import { useParams } from 'react-router-dom'
 import './App.css'
-import { Top } from './components/Top'
 
 function App() {
+  const { id } = useParams<{ id: string }>()
+
+  const value = (() => {
+    if (id) {
+      console.log(id)
+      // get value with id
+      return `value with ${id}`
+    }
+    console.log('no id')
+    // get latest value
+    return 'latest value'
+  })()
+  console.log(String(value))
   return (
     <div className="flex-1">
+      <p>
+        id: {value}, value: {value}
+      </p>
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <Top />
+        <PlateController>
+          <PlateEditor />
+        </PlateController>
       </section>
     </div>
   )
