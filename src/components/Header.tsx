6b91f354fc254e46e71fd62c934ac849dc0ea928
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-18 21:27:25 +0900

    header hight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,9 +24,9 @@ export function Header() {
   }
 
   return (
-    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90 font-sans font-thin">
+    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90 font-sans font-thin h-16">
       <div className="w-full max-w-7xl mx-auto px-4">
-        <div className="flex justify-between h-14 items-center">
+        <div className="flex justify-between h-16 items-center">
           {theme == 'light' || theme == 'system' ? (
             <Link to={'/'} className="flex flex-shrink-0 items-center ml-8">
               <img src="owl.png" className="h-12" />
