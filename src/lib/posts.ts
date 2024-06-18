9ba1875831f8404f8fd5ebd9d4fb4d2ef915dfaf
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-18 20:34:34 +0900

    fix fetch

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,33 +1,60 @@
 import { Json } from '@/types/supabase'
 import { supabase } from './supabase'
 
-export const fetchPostList = async (isLoggedIn?: boolean) => {
-  const postItems = await supabase
-    .from('post')
-    .select('id ,order, is_open')
-    .order(' order ', { ascending: false })
-  return postItems.data ?? []
+export const fetchPostList = async (isLoggedIn: boolean) => {
+  if (isLoggedIn) {
+    const postItems = await supabase
+      .from('post')
+      .select('id')
+      .order(' order ', { ascending: false })
+    return postItems.data ?? []
+  } else {
+    const postItems = await supabase
+      .from('post')
+      .select('id')
+      .eq('is_open', true)
+      .order(' order ', { ascending: false })
+    return postItems.data ?? []
+  }
 }
 
 export const fetchPost = async (id?: string) => {
   if (!id) {
     const post = await supabase
       .from('post')
-      .select('*')
       .select('id, order, content, is_open, created_at, updated_at')
+      .eq('is_open', true)
       .order(' order ', { ascending: false })
       .limit(1)
-      .single()
-    return post.data
+      .maybeSingle()
+    return post.data ?? undefined
   } else {
     const post = await supabase
       .from('post')
       .select('id, order, content, is_open, created_at, updated_at')
       .eq('id', id)
+      .eq('is_open', true)
+      .maybeSingle()
+    return post.data ?? undefined
+  }
+}
+
+export const fetchPostAdmin = async (id?: string) => {
+  if (!id) {
+    const post = await supabase
+      .from('post')
+      .select('id, order, content, is_open, created_at, updated_at')
       .order(' order ', { ascending: false })
       .limit(1)
-      .single()
-    return post.data
+      .maybeSingle()
+    return post.data ?? undefined
+  } else {
+    const post = await supabase
+      .from('post')
+      .select('id, order, content, is_open, created_at, updated_at')
+      .eq('id', id)
+      .maybeSingle()
+    return post.data ?? undefined
   }
 }
 
