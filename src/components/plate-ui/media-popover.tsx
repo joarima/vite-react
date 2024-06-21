a8e04908cc3741727cdd8f3a75dd6a09a58e5c33
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-22 07:36:12 +0900

    画像ドロップ時にdata url から File に変換

--- src/components/plate-ui/media-popover.tsx
+++ src/components/plate-ui/media-popover.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import {
   isSelectionExpanded,
+  setNodes,
+  useEditorRef,
   useEditorSelector,
   useElement,
   useRemoveNodeButton,
 } from '@udecode/plate-common'
+
 import {
+  ELEMENT_IMAGE,
   FloatingMedia as FloatingMediaPrimitive,
+  TMediaElement,
   floatingMediaActions,
   useFloatingMediaSelectors,
 } from '@udecode/plate-media'
@@ -15,6 +20,8 @@ import { useReadOnly, useSelected } from 'slate-react'
 
 import { Icons } from '@/components/icons'
 
+import { toast } from '@/components/ui/use-toast'
+import { dataURLtoFile } from '@/lib/image'
 import { Button, buttonVariants } from './button'
 import { CaptionButton } from './caption'
 import { inputVariants } from './input'
@@ -24,11 +31,13 @@ import { Separator } from './separator'
 export interface MediaPopoverProps {
   children: React.ReactNode
   pluginKey?: string
+  url?: string
 }
 
-export function MediaPopover({ children, pluginKey }: MediaPopoverProps) {
+export function MediaPopover({ children, pluginKey, url }: MediaPopoverProps) {
   const readOnly = useReadOnly()
   const selected = useSelected()
+  const isImage = pluginKey === ELEMENT_IMAGE
 
   const selectionCollapsed = useEditorSelector(
     (editor) => !isSelectionExpanded(editor),
@@ -46,6 +55,39 @@ export function MediaPopover({ children, pluginKey }: MediaPopoverProps) {
 
   const element = useElement()
   const { props: buttonProps } = useRemoveNodeButton({ element })
+
+  const [embedUrl, setEmbedUrl] = useState<string | undefined>(
+    isImage ? 'https://cdn.candycode.com/jotai/jotai-mascot.png' : undefined
+  )
+  const editor = useEditorRef()
+
+  const upload = () => {
+    if (!url) {
+      toast({
+        title: 'no file url.',
+      })
+      return
+    }
+    // data url => File
+    const file = dataURLtoFile(url)
+    if (!file) {
+      toast({
+        title: 'invalid file.',
+      })
+      return
+    }
+    // const s3Url = s3 url 取得
+    console.log(file)
+
+    // https://github.dev/udecode/plate/blob/main/packages/media/src/media/FloatingMedia/FloatingMediaUrlInput.tsx
+    // FloatingMediaPrimitive.UrlInput で Enter を押した時の処理と同様
+    // set
+    setNodes<TMediaElement>(editor, {
+      url: 'https://cdn.candycode.com/jotai/jotai-mascot.png',
+    })
+    // reset
+    floatingMediaActions.reset()
+  }
 
   if (readOnly) return <>{children}</>
 
@@ -88,6 +130,17 @@ export function MediaPopover({ children, pluginKey }: MediaPopoverProps) {
             <Button size="sms" variant="ghost" {...buttonProps}>
               <Icons.delete className="size-4" />
             </Button>
+            {isImage && (
+              <Button
+                variant="ghost"
+                className=""
+                onClick={() => {
+                  upload()
+                }}
+              >
+                upload
+              </Button>
+            )}
           </div>
         )}
       </PopoverContent>
