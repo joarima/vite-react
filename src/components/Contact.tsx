a21dcdd6637eb2c9fd111cc8b857932980044da7
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-24 11:07:54 +0900

    add github repo

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,7 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from '@/components/ui/hover-card'
+
 export function Contact() {
   return (
     <section>
@@ -35,6 +36,31 @@ export function Contact() {
                 </a>
               </h4>
               <p className="text-sm break-all">there are not many to say.</p>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button variant="link">@joarima</Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-auto max-w-xs">
+          <div className="flex justify-between space-x-4">
+            <Avatar>
+              <AvatarImage src="ghostsandvodka2.jpg" />
+              <AvatarFallback>VC</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <h4 className="text-sm font-semibold">
+                <a
+                  href="https://github.com/joarima/vite-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  @joarima/vite-react
+                </a>
+              </h4>
+              <p className="text-sm break-all">github repo.</p>
             </div>
           </div>
         </HoverCardContent>
